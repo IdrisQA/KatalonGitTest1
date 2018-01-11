f994948a25fa74f9c9dd0f171d9d61bdeef0d1d6
Mumeen <Mumeen@DESKTOP-RNU9SNV>
2018-01-11 15:14:28 +0100

    Added New Test to Tom

---
+++ Object Repository/Tom_OR/Page_Life Charger   a new beginning/a_One Act of Kindness Each Day.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_One Act of Kindness Each Day</name>
+   <tag></tag>
+   <elementGuidId>13d1ecc4-d977-4634-b9bc-8acd97545c32</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>http://lifecharger.org/one-act-of-kindness-each-day/</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>One Act of Kindness Each Day</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;post-1331&quot;)/header[@class=&quot;entry-header&quot;]/h2[1]/a[1]</value>
+   </webElementProperties>
+</WebElementEntity>
