4a687f366937b1f469917f7147b2306bf58554ce
MumeenIdris <idris@herlabytes.com>
2018-01-15 09:10:11 +0100

    Tom Test another product

---
+++ Test Suites/Tom Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Tom Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ec1348a3-d653-4f28-9439-920dc1924e5d</testSuiteGuid>
+</TestSuiteEntity>
